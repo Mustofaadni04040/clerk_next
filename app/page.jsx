71ade6551008ed8a459ab69435d25992d44b4892
mustofa adny <adniadni04@gmail.com>
2025-02-09 15:40:16 +0700

    protecting pages using middleware

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 export default async function Home() {
   const { userId } = await auth();
   const user = await currentUser();
-  console.log(user);
-  console.log(userId);
+  // console.log(user);
+  // console.log(userId);
   return <h1>Clerk Authentication</h1>;
 }
